0389cf3f349602d6a3ef695e05ebeb78ca7518b6
Amy Y <ctrlalternativedelete@gmail.com>
2020-06-20 23:58:27 +1000

    fixed up code to work more consistently

--- background.js
+++ background.js
@@ -1,15 +1,12 @@
     browser.tabs.onUpdated.addListener(handleUpdated);
 
     function handleUpdated(tabID, changeInfo, tabInfo) {
-
-            if(tabInfo.status == "loading") {
-                let newUrl = changeInfo.url
-                if (newUrl !== undefined){                    
-                    let command = ":~:text="
-                    if (newUrl.includes(command)){
-                        var parsedVariables = decodeURI(newUrl.slice(newUrl.indexOf(command) + command.length));
-                        setTimeout(function(){ browser.find.find(parsedVariables, {tabId: tabID}).then(found) }, 1000);
-                    }
+            if(changeInfo.status == "complete") {
+                let newUrl = tabInfo.url
+                let command = ":~:text="
+                if (newUrl.includes(command)){
+                    var parsedVariables = decodeURI(newUrl.slice(newUrl.indexOf(command) + command.length));
+                    browser.find.find(parsedVariables, {tabId: tabID}).then(found)
                 }
             }
 
